a34df589687c0d74345dc46f9c1a9b352500c7e0
mahdiASC <mahdi@allstarcode.org>
2017-06-12 11:37:14 -0400

    first commit

--- main.js
+++ main.js
@@ -9,23 +9,21 @@ function getParam(name){
     }
 }
 
-//Grabbing params
-var circleCount = getParam("circleCount") || 1;
-var colorPick = getParam("colorPick") || "red";
-var shapePick = getParam("shapePick") || "circle";
+
+//TASK//
+//Take 3 parameters from the href: 1) number, 2) shape, 3) color
+//In the "setup()" function, use the parameters to create a number of shapes with the given color
+//If a parameter is NOT found in the href, a default value should be set
 
 function setup(){
     createCanvas(400,400);
     background(123);
 
-    fill(colorPick);
-    if(shapePick == "circle"){
-        for(var i = 0; i<circleCount;i++){
-            ellipse(random(width),random(height),20,20);
-        }
-    }else if(shapePick == "square"){
-        for(var i = 0; i<circleCount;i++){
-            rect(random(width),random(height),20,20);
-        }
-    }
+    //Create a NUMBER of SHAPES with a COLOR
+
+}
+
+
+function draw(){
+
 }
