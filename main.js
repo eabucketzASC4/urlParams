e768502bc9c565686aff9aa1aa5a42208d87d452
mahdiASC <mahdi@allstarcode.org>
2017-06-12 11:39:36 -0400

    merging changes

--- main.js
+++ main.js
@@ -9,20 +9,30 @@ function getParam(name){
     }
 }
 
-
 //TASK//
 //Take 3 parameters from the href: 1) number, 2) shape, 3) color
 //In the "setup()" function, use the parameters to create a number of shapes with the given color
 //If a parameter is NOT found in the href, a default value should be set
+var circleCount = getParam("circleCount") || 1;
+var colorPick = getParam("colorPick") || "red";
+var shapePick = getParam("shapePick") || "circle";
 
 function setup(){
     createCanvas(400,400);
     background(123);
 
     //Create a NUMBER of SHAPES with a COLOR
-
+    fill(colorPick);
+    if(shapePick == "circle"){
+        for(var i = 0; i<circleCount;i++){
+            ellipse(random(width),random(height),20,20);
+        }
+    }else if(shapePick == "square"){
+        for(var i = 0; i<circleCount;i++){
+            rect(random(width),random(height),20,20);
+        }
+    }
 }
-
 
 function draw(){
 
