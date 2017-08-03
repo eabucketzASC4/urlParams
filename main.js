89ee94401fd22cacb7684497ddd2a6151e4d197d
Elijah Atwell <elijah_atwell@woodberry.org>
2017-08-03 10:27:40 -0700

    bob

--- main.js
+++ main.js
@@ -15,11 +15,20 @@ function getParam(name){
 //In the "setup()" function, use the parameters to create a number of shapes with the given color
 //If a parameter is NOT found in the href, a default value should be set
 
+var number = getParam('number') || 7;
+var shape = getParam('shape') || 'circle';
+var colour = getParam('colour') || 'white';
+
 function setup(){
     createCanvas(400,400);
     background(123);
-
+    fill(colour);//color of shapers
     //Create a NUMBER of SHAPES with a COLOR
+    if(shape === 'circle') {
+        for(i = 0; i < number; i++) {
+            ellipse(random(width),random(height),20,)
+        }
+    }
 
 }
 
